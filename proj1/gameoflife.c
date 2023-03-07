8502e25bb20cb2a501d6e9a0f2337632cd7adb7d
Seunghan Lee <shlee4458@gmail.com>
2023-03-07 14:44:52 -0500

    Complete: GameOfLife - proj1

--- proj1/gameoflife.c
+++ proj1/gameoflife.c
@@ -17,19 +17,93 @@
 #include <inttypes.h>
 #include "imageloader.h"
 
-//Determines what color the cell at the given row/col should be. This function allocates space for a new Color.
-//Note that you will need to read the eight neighbors of the cell in question. The grid "wraps", so we treat the top row as adjacent to the bottom row
+// Determines what color the cell at the given row/col should be. This function allocates space for a new Color.
+// Note that you will need to read the eight neighbors of the cell in question. 
+// The grid "wraps", so we treat the top row as adjacent to the bottom row
 //and the left column as adjacent to the right column.
 Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
 {
 	//YOUR CODE HERE
+	/* Get the number of cells alive in the 8 directions */
+	int ROW = image->rows;
+	int COL = image->cols;
+	int isAliveR, isAliveG, isAliveB; // whether R, G, B is 255 for the current cell
+	int numAliveR, numAliveG, numAliveB = 0; // number of cells alive for each channel
+	int idx = row * COL + col; // index of the current cell
+
+	/* Check if R, G, B value of the given cell is alive */
+	isAliveR = (image->image[idx]->R == 255);
+	isAliveG = (image->image[idx]->G == 255);
+	isAliveB = (image->image[idx]->B == 255);
+
+	for (int r = -1 + row; r <= 1 + row; r++) {
+		for (int c = -1 + col; c <= 1 + col; c++) {
+			if (r != row && c != col) {
+				int wrap_r = r % ROW; // wrap the index
+				int wrap_c = c % COL;
+
+				if (image->image[wrap_r * COL + wrap_c]->R == 255) { // if the R cell is alive
+					numAliveR += 1; // add one to alive
+				}
+
+				if (image->image[wrap_r * COL + wrap_c]->G == 255) { // if the R cell is alive
+					numAliveG += 1; // add one to alive
+				}
+
+				if (image->image[wrap_r * COL + wrap_c]->B == 255) { // if the R cell is alive
+					numAliveB += 1; // add one to alive
+				}
+			}
+		}
+	}
+
+	/* Get the digit in the rule */
+	// if the cell is alive, update number of shifts by 9 for each channel
+	numAliveR = 9 * isAliveR + numAliveR;
+	numAliveG = 9 * isAliveG + numAliveG;
+	numAliveB = 9 * isAliveB + numAliveB;
+
+	// using the number of cells alive, find the value from the rule
+	int live_or_deadR = (rule & (1 << numAliveR)) >> numAliveR; // mask the alive_th digit of the rule
+	int live_or_deadG = (rule & (1 << numAliveG)) >> numAliveG; // mask the alive_th digit of the rule
+	int live_or_deadB = (rule & (1 << numAliveB)) >> numAliveB; // mask the alive_th digit of the rule
+
+	// return the color with the value
+	Color *color = (Color *) malloc(sizeof(Color)); // create a new memory
+	color->R = live_or_deadR * 255;
+	color->G = live_or_deadG * 255;
+	color->B = live_or_deadB * 255;
+	printf("%d %d\n", row, col);
+	printf("%d\n", color->R);
+	return color;
 }
 
 //The main body of Life; given an image and a rule, computes one iteration of the Game of Life.
-//You should be able to copy most of this from steganography.c
 Image *life(Image *image, uint32_t rule)
 {
 	//YOUR CODE HERE
+	
+	/* Declare variables */
+	int idx;
+	int ROW = image->rows;
+	int COL = image->cols;
+
+	/* Create a new memory for return image */
+	Image *life_image = (Image *) malloc(sizeof(Image));
+	life_image->image = (Color **) malloc(sizeof(Color *) * ROW * COL);
+	life_image->rows = ROW;
+	life_image->cols = COL;
+
+	/* Get the life of the given image by looping over all the grid in the image and 
+	putting the Color instance in each grid */
+	for (int r = 0; r < ROW; r++) {
+		for (int c = 0; c < COL; c++) {
+			idx = r * COL + c;
+			life_image->image[idx] = evaluateOneCell(image, r, c, rule);
+		}
+	}
+
+	return life_image;
 }
 
 /*
@@ -50,4 +124,17 @@ You may find it useful to copy the code from steganography.c, to start.
 int main(int argc, char **argv)
 {
 	//YOUR CODE HERE
+	if (argc != 3) { // if number of arguments does not match return - 1
+		printf("%s", argv[1]);
+		return -1;
+	}
+
+	Image *image = readData(argv[1]); // read the image
+	uint32_t rule = strtol(argv[2], NULL, 16);
+	Image *life_image = life(image, rule); // convert to the lift image
+	writeData(life_image); // Print the life image
+
+	/* Free the memory */
+	freeImage(image);
+	freeImage(life_image);
 }
